b87cf4f5f92d0d12a6608c0b761f7c872592e41b
Abdelfatah Ashour <abdelfatahashour4@gmail.com>
2024-03-06 19:05:50 +0200

    Change naming variable secret

--- src/shared/lib/OAuth/oauthOption.ts
+++ src/shared/lib/OAuth/oauthOption.ts
@@ -1,10 +1,12 @@
 import { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+
 const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_ID!;
 const GOOGLE_CLIENT_SECRET = process.env.NEXT_PUBLIC_GOOGLE_SECRET!;
-const AUTH_NEXT_SECRET = process.env.NEXT_PUBLIC_NEXT_AUTH_SECRET!;
+const AUTH_SECRET = process.env.NEXTAUTH_SECRET!;
 
 export const authOptions: AuthOptions = {
+  secret: AUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: GOOGLE_CLIENT_ID ?? "",
@@ -14,5 +16,4 @@ export const authOptions: AuthOptions = {
   session: {
     strategy: "jwt",
   },
-  secret: AUTH_NEXT_SECRET,
 };
